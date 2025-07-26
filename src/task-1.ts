a793c64e7de162725da41db0572bf80cdd0c05a3
Anna Pastykhova <rainbow261@gmail.com>
2025-07-26 16:18:46 +0300

    Initial commit

---
+++ src/task-1.ts
@@ -0,0 +1,10 @@
+const name: string = "Alice";
+const age: Number = 30;
+const isOnline: boolean = true;
+
+console.log(`Name: ${name}`);
+console.log(`Age: ${age}`);
+console.log(`Online: ${isOnline}`);
+
+
+
